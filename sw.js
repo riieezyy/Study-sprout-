0d8e8a3f286547d52fe051c6204d9674b687a208
riieezyy <reesharout14@gmail.com>
2026-08-12 17:26:24 +0530

    Add files via upload
    update mimori service worker

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE="mimori-v3-1";
+const ASSETS=["./","./index.html","./manifest.webmanifest","./assets/mimori-icon.png",...[
+"mochi","boba","niko","pudding","luna","cloudie","peach","smokey","pika","berry"
+].map(x=>`./assets/buddies/${x}.png`)];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener("fetch",e=>{
+  if(e.request.method!=="GET")return;
+  e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)));
+});
